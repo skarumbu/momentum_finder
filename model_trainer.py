a9c9523c9ff80c6f15c09c46de1da1afa63c58c2
skarumbu <karumbunathan@gmail.com>
2025-03-22 11:37:52 -0700

    nitial Commit

--- model_trainer.py
+++ model_trainer.py
@@ -1,5 +1,3 @@
-# model_trainer.py
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, accuracy_score
